ed0698a45ad7eef68575c6655ecf65696a0d0470
Spoonman <155550137+JJKnegtel@users.noreply.github.com>
2024-07-10 10:58:05 +0200

    Update site-config.ts

--- lib/site-config.ts
+++ lib/site-config.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
-  pageTitle: 'The Grid | Data Explorer',
+  pageTitle: 'FarMap| Data Explorer',
   pageDescription:
-    'Powering Discoverability of Assets, Products, and Services in Web3.',
+    'Discover and explore the latest projects in Farcaster, Base and Degen ecosystems',
   filterByProductIds: [87, 88, 129], // Used to filter which blockchains to show in the full list.
   blockchainIds: [87, 88, 129, 21], // Filter default options. Filters the options of (productSupports, productDeployedOnFilter and assetDeployedOnFilter)
   blockchainProductTypeIds: [15, 16, 17] // Filter default options. used by productDeployedOnFilter.
